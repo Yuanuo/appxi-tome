076915499fd790a0c510e72076636704abbbe83c
Yuanuox <yuanuox@outlook.com>
2021-01-24 16:21:40 +0800

    添加一个实现用于将所有数据存到Map中以方便获取

--- src/main/java/org/appxi/tome/cbeta/BookTree.java
+++ src/main/java/org/appxi/tome/cbeta/BookTree.java
@@ -2,7 +2,9 @@ package org.appxi.tome.cbeta;
 
 import org.appxi.util.ext.Node;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class BookTree extends BookTreeBase<Node<CbetaBook>> {
     public BookTree(BookMap books, BookTreeMode mode) {
@@ -16,6 +18,26 @@ public class BookTree extends BookTreeBase<Node<CbetaBook>> {
 
     @Override
     protected void setTreeChildren(Node<CbetaBook> parent, List<Node<CbetaBook>> children) {
-        parent.children.addAll(children);
+        children.forEach(c -> c.setParent(parent));
+    }
+
+    public static class WithMap extends BookTree {
+        final Map<String, Node<CbetaBook>> dataMap = new HashMap<>(1024);
+
+        public WithMap(BookMap books, BookTreeMode mode) {
+            super(books, mode);
+        }
+
+        @Override
+        protected Node<CbetaBook> createTreeItem(CbetaBook itemValue) {
+            Node<CbetaBook> node = super.createTreeItem(itemValue);
+            if (null != itemValue && null != itemValue.id)
+                dataMap.put(itemValue.id, node);
+            return node;
+        }
+
+        public Map<String, Node<CbetaBook>> getDataMap() {
+            return dataMap;
+        }
     }
 }
